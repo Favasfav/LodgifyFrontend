067d6bae27f404857ff22745c9f06443c9bee9ba
Favasfav <favas.fav@gmail.com>
2023-12-06 21:43:24 +0530

    url changed versal ok

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,3 @@
 // export const baseUrl = 'http://s3.lodgify.boxwatches.shop/'
-export const baseUrl='http://127.0.0.1:8000/'
+export const baseUrl='https://lodgify-booking-knrrxcya4-favas-projects.vercel.app/'
 export const chatUrl='127.0.0.1:8000'
