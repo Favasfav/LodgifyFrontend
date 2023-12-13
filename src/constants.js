c7f26f3cad99892f73810151f991a929d93f04e5
Favasfav <favas.fav@gmail.com>
2023-12-13 12:38:29 +0530

    daphne hosted for chat

--- src/constants.js
+++ src/constants.js
@@ -2,7 +2,7 @@
 
 // export const baseUrl = 'http://s3.lodgify.boxwatches.shop/'
 // export const baseUrl='https://lodgify.boxwatches.shop'
-export const chatUrl='127.0.0.1:8000'
+export const chatUrl='lodgify.boxwatches.shop'
 
 export const baseUrl='https://lodgify.boxwatches.shop/'
 
