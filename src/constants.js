1a8389ba482e57af05b7937d85e95fb931b2db51
Favasfav <favas.fav@gmail.com>
2024-03-18 16:59:40 +0530

    change url

--- src/constants.js
+++ src/constants.js
@@ -2,9 +2,9 @@
 
 // export const baseUrl = 'http://s3.lodgify.boxwatches.shop/'
 // export const baseUrl='https://lodgify.boxwatches.shop'
-export const chatUrl='127.0.0.1:8000'
-// export const chatUrl='lodgify.boxwatches.shop'
+// export const chatUrl='127.0.0.1:8000'
+export const chatUrl='lodgify.boxwatches.shop'
 
-// export const baseUrl='https://lodgify.boxwatches.shop/'
+export const baseUrl='https://lodgify.boxwatches.shop/'
 
-export const baseUrl='http://127.0.0.1:8000/'
+// export const baseUrl='http://127.0.0.1:8000/'
